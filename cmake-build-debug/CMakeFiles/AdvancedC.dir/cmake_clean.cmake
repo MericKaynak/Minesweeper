d33c787da493da60f51918b6c6aef71533de35da
mekay007 <meric.kaynak@stud.hn.de>
2024-07-16 20:00:16 +0200

    Game erstellen + spielen funktioniert

---
+++ cmake-build-debug/CMakeFiles/AdvancedC.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "AdvancedC"
+  "AdvancedC.pdb"
+  "CMakeFiles/AdvancedC.dir/main.cpp.o"
+  "CMakeFiles/AdvancedC.dir/main.cpp.o.d"
+  "CMakeFiles/AdvancedC.dir/src/Game.cpp.o"
+  "CMakeFiles/AdvancedC.dir/src/Game.cpp.o.d"
+  "CMakeFiles/AdvancedC.dir/src/GameRenderer.cpp.o"
+  "CMakeFiles/AdvancedC.dir/src/GameRenderer.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdvancedC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
